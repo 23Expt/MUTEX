13e0f27ab19ecfc8fdaaba65ec964704ad144644
Al Fachri Sagianto <163378556+23Expt@users.noreply.github.com>
2024-06-14 20:29:29 +0700

    Update ipc.c

--- ipc.c
+++ ipc.c
@@ -1,8 +1,11 @@
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
 #include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 #define PIPE_NAME "/tmp/mypipe"
 
@@ -19,7 +22,12 @@ void pembaca() {
 
     while (1) {
         // Membaca dari named pipe
-        read(fd, buffer, 1024);
+        ssize_t bytes_read = read(fd, buffer, 1024);
+        if (bytes_read == -1) {
+            perror("read");
+            exit(1);
+        }
+        buffer[bytes_read] = '\0'; // Add null terminator
         printf("Pembaca menerima: %s\n", buffer);
     }
 
@@ -39,7 +47,11 @@ void penulis() {
 
     for (int i = 1; i <= 5; i++) {
         sprintf(buffer, "Pesan %d", i);
-        write(fd, buffer, strlen(buffer));
+        ssize_t bytes_written = write(fd, buffer, strlen(buffer));
+        if (bytes_written == -1) {
+            perror("write");
+            exit(1);
+        }
         sleep(1);
     }
 
